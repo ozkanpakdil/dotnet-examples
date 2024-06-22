fff3b7740102066129cb9bda1384be3e03932362
ozkanpakdil <ozkan.pakdil@gmail.com>
2024-06-22 17:30:25 +0100

    custom container test added

---
+++ custom-testconatiner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotNet.Testcontainers;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+
+ConsoleLogger.Instance.DebugLogLevelEnabled = true;
+
+var container = new ContainerBuilder()
+    .WithImage("ozkanpakdil/mssql-ubuntu")
+    .WithEnvironment("TERM", "xterm")
+    .Build();
+
+try
+{
+    await container.StartAsync();
+    
+    var logs = await container.GetLogsAsync();
+    Console.WriteLine("Container logs:");
+    Console.WriteLine(logs);
+
+    var execResult = await container.ExecAsync(new[] { "/bin/bash", "-c", "ls" });
+    Console.WriteLine($"Exec exit code: {execResult.ExitCode}");
+    Console.WriteLine($"Exec output: {execResult.Stdout}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
+finally
+{
+    await container.DisposeAsync();
+}
---
+++ custom-testconatiner/readme.md
@@ -0,0 +1,2 @@
+﻿# how to run the code
+`dotnet run` this will check out a docker image and run ls command on it then exits.
